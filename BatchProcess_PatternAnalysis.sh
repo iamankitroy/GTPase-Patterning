40ba5dd1ca1b4b8d48d240f399d81d4866c8a590
iamankitroy <intellect.ankit@gmail.com>
2023-06-20 14:15:54 +0200

    Updated batch processing script to use background data file as input for the GTPase pattern analysis script.

--- BatchProcess_PatternAnalysis.sh
+++ BatchProcess_PatternAnalysis.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
+# List image files
 function list_files()
 {
 	ls *Lipid.tif > filenames.temp
 }
 
+# Run segmentation script
 function segment_images()
 {
 	for filename in `cat filenames.temp`
@@ -13,27 +15,31 @@ function segment_images()
 	done
 }
 
+# Get file prefixes
 function get_file_prefixes()
 {
 	sed "s/C=.*//g" filenames.temp > prefixes.temp
 }
 
+# Analyze pattern statistics
 function analyse_patterns()
 {
 	get_file_prefixes
 
 	for prefix in `cat prefixes.temp`
 	do
-		./GTPase_patterning_analysis.py $prefix\C=Lipid_segmented.tif $prefix\C=Lipid.tif $prefix\C=Total.tif $prefix\C=Active.tif
+		./GTPase_patterning_analysis.py $prefix\C=Lipid_segmented.tif $prefix\C=Lipid.tif $prefix\C=Total.tif $prefix\C=Active.tif background_dict.p
 		echo Analysed $prefix
 	done
 }
 
+# Remove temporary files
 function clean_up()
 {
 	rm *.temp
 }
 
+# Automate steps
 function automate()
 {
 	list_files
@@ -45,4 +51,10 @@ function automate()
 	clean_up
 }
 
+# Run automation
 automate
+
+# Ankit Roy
+# 28th April, 2023
+# 20th June, 2023			>>		Updated script to accomodate changes to the GTPase pattern analysis script.
+#							>>		Added comments.
