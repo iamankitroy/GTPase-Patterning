b71beefd34be04b231710995279e64d600275e8d
iamankitroy <intellect.ankit@gmail.com>
2024-05-10 12:56:29 +0200

    Added a script to count the number of GTPase and GDI tracks

---
+++ track_count.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+# Track counter
+counter()
+{
+	count=`sed '/^#/d' $1 | sed -n "/,$2$/p" | cut -d',' -f3 | sort | uniq | wc -l`
+}
+
+# Cleaner
+cleanup()
+{
+	sed -i "s/ //g" $1
+}
+
+# Main function
+main()
+{
+	echo "Filename,n_GTPase,n_GDI" | tee -a ncounts.csv
+
+	for i in `ls *colocalisation.csv`
+	do
+		counter "$i" "GTPase"
+		gtpase_count=$count			# GTPase track count
+
+		counter "$i" "GDI"
+		gdi_count=$count			# GDI track count
+
+		echo "$i,$gtpase_count,$gdi_count" | tee -a ncounts.csv
+	done
+
+	cleanup ncounts.csv
+}
+
+# Run main
+main
